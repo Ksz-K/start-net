63a3c761a998b39807b40a9a6cdb0f614019beb7
Krzysztof Szachna-Krause <kszk@gmx.us>
2020-05-18 12:49:31 +0200

    DB connection established

--- server.js
+++ server.js
@@ -1,6 +1,15 @@
+const path = require("path");
 const express = require("express");
+const dotenv = require("dotenv");
+const connectDB = require("./config/db");
 
 const app = express();
+
+//Load env vars
+dotenv.config({ path: "./config/config.env" });
+
+//Connect DB from Cloud
+connectDB();
 
 app.get("/", (req, res) => {
   res.send("API is up");

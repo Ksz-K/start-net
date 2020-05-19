0027698ecb9450ffc247e319f8feeb03a4088bde
Krzysztof Szachna-Krause <kszk@gmx.us>
2020-05-19 13:41:16 +0200

    Post model added

---
+++ models/Post.js
@@ -0,0 +1,54 @@
+const mongoose = require("mongoose");
+
+const PostSchema = new mongoose.Schema({
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+  },
+  text: {
+    type: String,
+    required: [true, "Post title cannot be empty"],
+  },
+  name: {
+    type: String,
+  },
+  avatar: {
+    type: String,
+  },
+  likes: [
+    {
+      user: {
+        type: Schema.Types.ObjectId,
+        ref: "User",
+      },
+    },
+  ],
+  comments: [
+    {
+      user: {
+        type: Schema.Types.ObjectId,
+        ref: "User",
+      },
+      text: {
+        type: String,
+        required: [true, "Post cannot be empty"],
+      },
+      name: {
+        type: String,
+      },
+      avatar: {
+        type: String,
+      },
+      createdAt: {
+        type: Date,
+        default: Date.now,
+      },
+    },
+  ],
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+module.exports = Post = mongoose.model("Post", PostSchema);
